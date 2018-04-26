2439dd35470cb4fc8f41a30cb887fa9b60b2ca02
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-26 22:07:19 +0200

    Fix itemLore récupération

--- src/main/java/fr/pmk_lobbyutils/navbarconnect/HotBarListener.java
+++ src/main/java/fr/pmk_lobbyutils/navbarconnect/HotBarListener.java
@@ -12,8 +12,12 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import fr.pmk_lobbyutils.navbarconnect.bungee.HotBarBungee;
 
 
 public class HotBarListener implements Listener {
@@ -21,6 +25,12 @@ public class HotBarListener implements Listener {
 	private static List<Player> playerList = new ArrayList<Player>(); 
 	private static List<HotBarData> hotBarList;
 	private static boolean isInit = false;
+	
+	private void removeQueuePlayer(Player p) {
+		if(playerList.contains(p)) {
+			playerList.remove(p);
+		}
+	}
 	
 	@EventHandler
 	public void OnPlayerJoin(PlayerJoinEvent e) {
@@ -57,12 +67,67 @@ public class HotBarListener implements Listener {
 		
 		// envoie des requètes
 		
+		ItemStack item = e.getItem();
+		ItemMeta itemMeta = item.getItemMeta();
+		String itemLore = "";
+		
+		Player p = e.getPlayer();
+		
+		if(itemMeta.hasLore()) {
+			
+			for (String l : itemMeta.getLore()) {
+				itemLore += l;
+			}
+			
+			System.out.println(itemLore);
+			
+			for (HotBarData hotBarData : hotBarList) {
+				if(itemLore.contains(hotBarData.getCode())) {
+					System.out.println("correspondence");
+					
+					
+					
+					if(hotBarData.isServerState()) {
+						// dispo
+						if(p.hasPermission(hotBarData.getPermission() + ".join")) {
+							// requete de connexion
+							p.sendMessage("demande de co");
+							HotBarBungee.connectServer(p, hotBarData.getServerName());
+							
+						}else {
+							//pas la permission
+							p.sendMessage("pas la permission");
+							
+							removeQueuePlayer(p);
+							e.setCancelled(true);
+							return;
+						}
+						
+					}else {
+						//pas dispo
+						p.sendMessage("pas dispo");
+						
+						removeQueuePlayer(p);
+						e.setCancelled(true);
+						return;
+						
+					}
+				}
+			}
+			removeQueuePlayer(p);
+			e.setCancelled(true);				
+			
+		}
+		
+		removeQueuePlayer(p);
+		e.setCancelled(true);
 		
 	}
 	
 	@EventHandler
 	public void OnPlayerQuit(PlayerQuitEvent e) {
-		
+		Player p = e.getPlayer();
+		removeQueuePlayer(p);
 	}
 	
 	@EventHandler
