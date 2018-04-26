f64bdc9846941a519a2920508869e53e5d2c5439
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-26 15:29:35 +0200

    Fix listener & join/quit message

--- src/main/java/fr/pmk_lobbyutils/listener/PlayerListener.java
+++ src/main/java/fr/pmk_lobbyutils/listener/PlayerListener.java
@@ -2,11 +2,11 @@ package fr.pmk_lobbyutils.listener;
 
 import java.util.List;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import fr.pmk_lobbyutils.config.Config;
 
@@ -14,23 +14,29 @@ public class PlayerListener implements Listener {
 
 	@EventHandler
 	public void OnPlayerJoin(PlayerJoinEvent e) {
-		System.out.println(e.getEventName());
 		
 		Player p = e.getPlayer();
+		String pUUID = p.getUniqueId().toString();
 		String pName = p.getDisplayName();
 		
 		List<String> listPlayer = Config.getPlayerList();
 		
-		if(listPlayer.contains(pName)) {
+		if(!listPlayer.contains(pUUID)) {
 			
+			listPlayer.add(pUUID);
 			//affichage du message de bienvenue
-			e.setJoinMessage(ChatColor.MAGIC + "" + ChatColor.GOLD + "abcd" + ChatColor.RESET + "" + ChatColor.BOLD + "" + ChatColor.YELLOW + " " + pName + " " 
-							+ ChatColor.RESET + "" + ChatColor.GOLD + " bienvenue à vous sur le serveur PumpMyKins " + ChatColor.MAGIC + "abcd");
+			e.setJoinMessage("§2§kab§r" + "§6Bienvenue à §e§l" + pName + "§r§6, le §b§l" + (listPlayer.size()-1) + "§r§6ème joueurs ! §2§kab");
+			
+			// set un grade
 			
 		}else {
 
-			e.setJoinMessage("Souhaitez un bon retour à {player} sur le serveur");
+			e.setJoinMessage("");	// pas de message
 			
 		}
-	}	
+	}
+	
+	public void OnPlayerQuit(PlayerQuitEvent e) {
+		e.setQuitMessage("");		//pas de message
+	}
 }
