e1f9e67bda1188cc5737bd11b27667d232e61329
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-26 20:53:32 +0200

    Fix fields & getter/setter

--- src/main/java/fr/pmk_lobbyutils/navbarconnect/HotBarData.java
+++ src/main/java/fr/pmk_lobbyutils/navbarconnect/HotBarData.java
@@ -4,12 +4,38 @@ import org.bukkit.inventory.ItemStack;
 
 public class HotBarData {
 
+	
 	private int slot;
 	private ItemStack itemStack;
+	private boolean serverState = true;
+	private String downRaison = "";
+	private String permission = "";
 
+	
 	public HotBarData(int s, ItemStack i) {
 		this.setSlot(s);
 		this.itemStack = i;
+	}
+	
+	public HotBarData(int s, ItemStack i, String perm) {
+		this.setSlot(s);
+		this.itemStack = i;
+		this.permission = perm;
+	}
+	
+	public HotBarData(int s, ItemStack i, boolean state, String raison) {
+		this.setSlot(s);
+		this.itemStack = i;
+		this.serverState = state;
+		this.downRaison = raison;
+	}
+	
+	public HotBarData(int s, ItemStack i, boolean state, String raison, String perm) {
+		this.setSlot(s);
+		this.itemStack = i;
+		this.serverState = state;
+		this.downRaison = raison;
+		this.permission = perm;
 	}
 
 	public int getSlot() {
@@ -26,6 +52,30 @@ public class HotBarData {
 
 	public void setItemStack(ItemStack itemStack) {
 		this.itemStack = itemStack;
+	}
+
+	public boolean isServerState() {
+		return serverState;
+	}
+
+	public void setServerState(boolean serverState) {
+		this.serverState = serverState;
+	}
+
+	public String getDownRaison() {
+		return downRaison;
+	}
+
+	public void setDownRaison(String downRaison) {
+		this.downRaison = downRaison;
+	}
+
+	public String getPermission() {
+		return permission;
+	}
+
+	public void setPermission(String permission) {
+		this.permission = permission;
 	}
 	
 }
