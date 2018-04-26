129c25c73a4b43191442b5b3f34e0c88508e31e6
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-26 22:54:33 +0200

    Ajout de class de commandes HotBarConnect

---
+++ src/main/java/fr/pmk_lobbyutils/HotBarSetCommand.java
@@ -0,0 +1,15 @@
+package fr.pmk_lobbyutils;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class HotBarSetCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
