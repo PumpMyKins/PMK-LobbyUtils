6e3c48cfae267e4f79161d2d47d125edadb4b4e4
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-04-30 00:08:43 +0200

    Fix world tp on join & allow flight

--- src/main/java/fr/pmk_lobbyutils/listener/PlayerListener.java
+++ src/main/java/fr/pmk_lobbyutils/listener/PlayerListener.java
@@ -1,22 +1,27 @@
 package fr.pmk_lobbyutils.listener;
 
+import java.util.HashMap;
 import java.util.List;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import fr.pmk_lobbyutils.config.Config;
 
 public class PlayerListener implements Listener {
-
+	
+	private static HashMap<Player, String> hPlayerFlight = new HashMap<>();
+	
 	@EventHandler
 	public void OnPlayerJoin(PlayerJoinEvent e) {
 		
 		Player p = e.getPlayer();
-		String pUUID = p.getUniqueId().toString();
+		/*String pUUID = p.getUniqueId().toString();
 		String pName = p.getDisplayName();
 		
 		List<String> listPlayer = Config.getPlayerList();
@@ -33,11 +38,27 @@ public class PlayerListener implements Listener {
 
 			e.setJoinMessage("");	// pas de message
 			
-		}
+		}*/
+		
+		p.teleport(new Location(p.getWorld(), -537, 29, 1372));
+		e.setJoinMessage("");
+		p.setAllowFlight(true);
+		//hPlayerFlight.put(p, "2222223");
+		
+		
 	}
 	
 	@EventHandler
 	public void OnPlayerQuit(PlayerQuitEvent e) {
+		e.getPlayer().setAllowFlight(false);
 		e.setQuitMessage("");		//pas de message
+	}
+
+	public static HashMap<Player, String> gethPlayerFlight() {
+		return hPlayerFlight;
+	}
+
+	public static void sethPlayerFlight(HashMap<Player, String> hPlayerFlight) {
+		PlayerListener.hPlayerFlight = hPlayerFlight;
 	}
 }
