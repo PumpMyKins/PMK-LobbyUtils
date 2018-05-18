fd6d92f1db0fce2b0f08a459186e0d878a9cf59b
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-18 23:37:27 +0200

    Ajout du listener useItem à l'interface

--- src/main/java/fr/pmk_lobbyutils/hotbarconnectv2/utils/IHotBarConnectPlayerListener.java
+++ src/main/java/fr/pmk_lobbyutils/hotbarconnectv2/utils/IHotBarConnectPlayerListener.java
@@ -1,15 +1,18 @@
-package fr.pmk_lobbyutils.hotbarconnectv2;
+package fr.pmk_lobbyutils.hotbarconnectv2.utils;
 
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
-public interface IHotBarConnectPlayerListener {
+public interface IHotBarConnectPlayerListener{
 
 	public void OnPlayerJoin(PlayerJoinEvent event);
 	
 	public void OnPlayerLeave(PlayerQuitEvent event);
 	
-	public void OnPlayerDeath(PlayerDeathEvent event);
+	public void OnPlayerDeath(PlayerRespawnEvent event);
+	
+	public void OnPlayerUseItem(PlayerInteractEvent event);
 	
 }
