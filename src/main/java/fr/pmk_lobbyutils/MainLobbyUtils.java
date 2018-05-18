8d17042acc1c05b63db60668cd65d3bc5dd7b829
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-18 23:38:02 +0200

    Ajout call hotbarmanager dans Main onEnable

--- src/main/java/fr/pmk_lobbyutils/MainLobbyUtils.java
+++ src/main/java/fr/pmk_lobbyutils/MainLobbyUtils.java
@@ -3,10 +3,10 @@ package fr.pmk_lobbyutils;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import fr.pmk_lobbyutils.config.Config;
-import fr.pmk_lobbyutils.hotbarconnect.HotBarListener;
-import fr.pmk_lobbyutils.hotbarconnect.HotBarManager;
 import fr.pmk_lobbyutils.hotbarconnect.commands.HotBarListCommand;
 import fr.pmk_lobbyutils.hotbarconnect.commands.HotBarSetCommand;
+import fr.pmk_lobbyutils.hotbarconnectv2.HotBarManager;
+import fr.pmk_lobbyutils.hotbarconnectv2.listener.HotBarListener;
 import fr.pmk_lobbyutils.listener.PlayerListener;
 
 
@@ -14,6 +14,8 @@ public class MainLobbyUtils extends JavaPlugin{
 	
 	private static Config conf;
 	private static MainLobbyUtils instance;
+	
+	private static HotBarManager hotBarManager;
 
 	@Override
 	public void onEnable() {
@@ -26,13 +28,18 @@ public class MainLobbyUtils extends JavaPlugin{
 								
 		conf.initDataFolder();
 		conf.initAndGetFile("config.yml");	// init config default file
-		HotBarManager.initServerItem();
 		
 		this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
 		
 		// Ajout du listener
 		getServer().getPluginManager().registerEvents(new PlayerListener(), this);
-		getServer().getPluginManager().registerEvents(new HotBarListener(), this);
+		
+		hotBarManager = HotBarManager.getManager(this);
+		
+		hotBarManager.setPlayerListener(new HotBarListener());
+		
+		
+		//getServer().getPluginManager().registerEvents(new HotBarListener(), this); hold hotbar system
 		
 		this.getCommand("hbclist").setExecutor(new HotBarListCommand());
 		this.getCommand("hbcserver").setExecutor(new HotBarSetCommand());
@@ -59,6 +66,14 @@ public class MainLobbyUtils extends JavaPlugin{
 
 	public static Plugin get() {
 		return instance;
+	}
+
+	public static HotBarManager getHotBarManager() {
+		return hotBarManager;
+	}
+
+	public static void setHotBarManager(HotBarManager hotBarManager) {
+		MainLobbyUtils.hotBarManager = hotBarManager;
 	}
 	
 }
