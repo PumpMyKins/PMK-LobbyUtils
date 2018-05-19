0b3d75308cfbe329fa9276cb312d8bbe42d317e7
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-19 18:09:27 +0200

    Fix import remove methods

--- src/main/java/fr/pmk_lobbyutils/MainLobbyUtils.java
+++ src/main/java/fr/pmk_lobbyutils/MainLobbyUtils.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
@@ -14,7 +15,9 @@ import fr.pmk_lobbyutils.hotbarconnect.commands.HotBarSetCommand;
 import fr.pmk_lobbyutils.hotbarconnectv2.HotBarManager;
 import fr.pmk_lobbyutils.hotbarconnectv2.inventory.HotBarInventory;
 import fr.pmk_lobbyutils.hotbarconnectv2.inventory.HotBarItemData;
+import fr.pmk_lobbyutils.hotbarconnectv2.listener.GameMenuItemListener;
 import fr.pmk_lobbyutils.hotbarconnectv2.listener.HotBarListener;
+import fr.pmk_lobbyutils.hotbarconnectv2.listener.InfoMenuItemListener;
 import fr.pmk_lobbyutils.hotbarconnectv2.listener.RagnaItemListener;
 import fr.pmk_lobbyutils.listener.PlayerListener;
 
@@ -45,34 +48,67 @@ public class MainLobbyUtils extends JavaPlugin{
 		
 		hotBarManager = HotBarManager.getManager(this);
 		
+		hotBarManager.buildMenu();
+		
 		hotBarManager.setDefaultInventory(createHotBarInv(hotBarManager));
 		hotBarManager.setPlayerListener(new HotBarListener());
 		
+		//this.getCommand("hbclist").setExecutor(new HotBarListCommand());
+		//this.getCommand("hbcserver").setExecutor(new HotBarSetCommand());
 		
 		//getServer().getPluginManager().registerEvents(new HotBarListener(), this); hold hotbar system
 		
-		this.getCommand("hbclist").setExecutor(new HotBarListCommand());
-		this.getCommand("hbcserver").setExecutor(new HotBarSetCommand());
+		//this.getCommand("hbclist").setExecutor(new HotBarListCommand());
+		//this.getCommand("hbcserver").setExecutor(new HotBarSetCommand());
 		
 	}
 	
 	private HotBarInventory createHotBarInv(HotBarManager h) {
 		
+		// item de connexion 
 		ItemStack iragna = new ItemStack(Material.APPLE);
 		ItemMeta iragnaM = iragna.getItemMeta();
 		
 		iragnaM.setDisplayName("§n§2PumpMyKins§9§o#1 §r§2§nRagnaMod-IV§4§l");
 		
-		List<String> l = new ArrayList<>();
-		l.add("§n§2PumpMyKins§9§o#1 §r§2§nRagnaMod-IV §r§f#r1pmk");
+		List<String> loreRagna = new ArrayList<>();
+		loreRagna.add("§n§2PumpMyKins§9§o#1 §r§2§nRagnaMod-IV §r§f#r1pmk");
 		
-		iragnaM.setLore(l);
+		iragnaM.setLore(loreRagna);
 		
 		iragna.setItemMeta(iragnaM);
 		
 		HotBarItemData itemDataRagna = new HotBarItemData("§n§2PumpMyKins§9§o#1 §r§2§nRagnaMod-IV§4§l", 2 , iragna , new RagnaItemListener());
 		
-		HotBarInventory inv = HotBarInventory.build(itemDataRagna);
+		ItemStack iInfoMenu = new ItemStack(Material.BOOK);
+		ItemMeta iInfoMenuM = iInfoMenu.getItemMeta();
+		
+		iInfoMenuM.setDisplayName("§9§l§nMenu d'information");
+		
+		List<String> loreInfoMenu = new ArrayList<>();
+		loreInfoMenu.add("§9§l§nMenu d'information §r§f#impmk");
+		
+		iInfoMenuM.setLore(loreInfoMenu);
+		
+		iInfoMenu.setItemMeta(iInfoMenuM);
+		
+		HotBarItemData itemDataInfoMenu = new HotBarItemData("§9§l§nMenu d'information", 4 , iInfoMenu , new InfoMenuItemListener());
+		
+		ItemStack iGameMenu = new ItemStack(Material.FIREWORK);
+		ItemMeta iGameMenuM = iInfoMenu.getItemMeta();
+		
+		iGameMenuM.setDisplayName("§6§l§nMenu de jeux");
+		
+		List<String> loreGameMenu = new ArrayList<>();
+		loreGameMenu.add("§6§l§nMenu de jeux §r§f#igpmk");
+		
+		iGameMenuM.setLore(loreGameMenu);
+		
+		iGameMenu.setItemMeta(iGameMenuM);
+		
+		HotBarItemData itemDataGameMenu = new HotBarItemData("§6§l§nMenu de jeux", 6 , iGameMenu , new GameMenuItemListener());
+		
+		HotBarInventory inv = HotBarInventory.build(itemDataRagna,itemDataInfoMenu,itemDataGameMenu);
 		
 		return inv;
 		
