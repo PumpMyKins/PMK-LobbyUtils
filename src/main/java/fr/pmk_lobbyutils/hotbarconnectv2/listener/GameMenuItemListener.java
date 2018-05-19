0ca203ee07f1e48f444eb425fd2bda416f633acc
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-19 15:03:46 +0200

    AJout des classes listeners pour les items

---
+++ src/main/java/fr/pmk_lobbyutils/hotbarconnectv2/listener/GameMenuItemListener.java
@@ -0,0 +1,15 @@
+package fr.pmk_lobbyutils.hotbarconnectv2.listener;
+
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import fr.pmk_lobbyutils.hotbarconnectv2.utils.IHotBarItemListener;
+
+public class GameMenuItemListener implements IHotBarItemListener {
+
+	@Override
+	public void callItem(PlayerInteractEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
