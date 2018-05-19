7282a0b2454a6ce31ae04ca880fb0f6b576fff21
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-19 13:59:08 +0200

    Fix import & ajout setPlayerInventory method

--- src/main/java/fr/pmk_lobbyutils/hotbarconnectv2/HotBarManager.java
+++ src/main/java/fr/pmk_lobbyutils/hotbarconnectv2/HotBarManager.java
@@ -2,9 +2,11 @@ package fr.pmk_lobbyutils.hotbarconnectv2;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
 
 import fr.pmk_lobbyutils.MainLobbyUtils;
 import fr.pmk_lobbyutils.hotbarconnectv2.inventory.HotBarInventory;
+import fr.pmk_lobbyutils.hotbarconnectv2.inventory.HotBarItemData;
 
 public class HotBarManager {
 
@@ -44,6 +46,17 @@ public class HotBarManager {
 	public void setInventory(Player player) {
 		// TODO Auto-generated method stub
 		//Inventory inv = HotBarInventory.getBuilded();
+		
+		HotBarInventory hotBarInv = this.hotBarInv;
+		
+		Inventory i = player.getInventory();
+		i.clear();
+		
+		for (HotBarItemData itemData : hotBarInv) {
+			
+			i.setItem(itemData.getIndex(), itemData.getItem());
+			
+		}
 		
 	}
 
