fbb3c069182bdefebdd3202706259ef5018ab558
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-05-19 13:48:08 +0200

    Fix listenre parameter

--- src/main/java/fr/pmk_lobbyutils/hotbarconnectv2/utils/IHotBarItemListener.java
+++ src/main/java/fr/pmk_lobbyutils/hotbarconnectv2/utils/IHotBarItemListener.java
@@ -1,7 +1,9 @@
 package fr.pmk_lobbyutils.hotbarconnectv2.utils;
 
+import org.bukkit.event.player.PlayerInteractEvent;
+
 public interface IHotBarItemListener {
 
-	public void callItem();
+	public void callItem(PlayerInteractEvent event);
 	
 }
